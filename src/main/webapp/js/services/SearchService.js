831294f913aa1b1abe66713ff991ac95b064d328
csardo <christina.sardo@gmail.com>
2016-05-24 09:03:16 -0700

    update item hard code working

--- src/main/webapp/js/services/SearchService.js
+++ src/main/webapp/js/services/SearchService.js
@@ -1,8 +1,20 @@
 angular.module('DataSaints').factory('searchService', function($http) {
 
     var findItem = function(item) {   	
-        // Angular $http() and then() both return promises themselves 
-        return $http.post('/datasaints/findItem/', item)
+        // Angular $http() and then() both return promises themselves
+    	if (item.serial == null) 
+    		item.serial = -1;
+    	if (status == null)
+    		item.status = "NONE";
+    	
+    	console.log('searching for');
+    	console.log(item);
+    	
+    	var item2 = {
+    			id: "1",
+    			serial: -1
+    	};
+        return $http.post('/datasaints/findItem/', item2)
     };
 
     return { findItem: findItem };
